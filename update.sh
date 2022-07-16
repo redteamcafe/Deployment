efcf391c833044bebe4f7d54ba54036d553d6639
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 01:09:32 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,13 +2,15 @@
 
 if
 #apt-get
-        [[ "$OSINFO" == "$APT" ]]; then
+        [[ "$OSINFO" == "$APT" ]];
+then
         UPDATE="apt update";
         UPGRADE="apt upgrade -y";
         INSTALL="apt install -y"
 elif
 #pacman
-        [[ "$OSINFO" == "$PAC" ]]; then
+        [[ "$OSINFO" == "$PAC" ]];
+then
         UPDATE="sleep 0";
         UPGRADE="pacman -Syu --noconfirm";
         INSTALL="pacman -S --noconfirm";
