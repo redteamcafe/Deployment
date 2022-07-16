92b08dc69c8fe64a6a4237ad6feb6b05f705fe4c
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 01:27:16 -0400

    Update addmin.sh

--- addmin.sh
+++ addmin.sh
@@ -2,15 +2,5 @@
 
 # This script changes the current unaem of the user to the desired one
 # This is useful for Arch Linux since the preinstalled qcow file has the default user 'arch'
-echo "1"
-UNAME=`whoami`
-echo "2"
-OLD_UNAME=$UNAME
-echo "3"
-echo "Enter the desired username >> "
-echo "4"
-echo "Changing default uname $UNAME"
-echo "5"
-sudo usermod -l $NEW_UNAME -m -d /home/$NEW_UNAME $OLD_UNAME
-echo "6"
-sudo groupmod -n $NEW_UNAME $OLD_UNAME
+
+echo "Enter the desired username >> " NEW_UNAME
