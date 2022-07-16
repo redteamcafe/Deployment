fa1b0ba563b3c78dbe77370a797616ad461a1ef9
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 17:32:20 -0400

    Update osinfo.sh

--- osinfo.sh
+++ osinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-APT=apt-get
-PAC=pacman
+export APT=apt-get
+export PAC=pacman
 
 declare -A osInfo;
 osInfo[/etc/redhat-release]=yum
@@ -17,21 +17,6 @@ do
     fi
 done
 
-if
-#apt-get
-        [[ "$OSINFO" == "$APT" ]]; then
-        UPDATE="apt update";
-        UPGRADE="apt upgrade -y";
-        INSTALL="apt install -y"
-        sudo $UPDATE ; sudo $UPGRADE
-elif
-#pacman
-        [[ "$OSINFO" == "$PAC" ]]; then
-        UPDATE="sleep 0";
-        UPGRADE="pacman -Syu --noconfirm";
-        INSTALL="pacman -S --noconfirm";
-else
-        sleep 0
-fi
+
 
 
