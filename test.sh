6627c0b224f5f1ee943343ebc0676ce06ff0bb24
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 18:38:20 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
-echo "TEST"
+# This will help automate some of the deployment of severs
+# Currently supports Debian and Arch Linux
+
+#STEP: Check mount location
+#NOTE: This was originally used when I would mount directory where the script lied on the server directly
+#NOTE: This is being kept for preservation purposes
+##export SCRIPTDIR=$(readlink -f "$0")
+##export SCRIPTPATH=$(dirname "$SCRIPTDIR")
+##export PWD=$(echo $SCRIPTPATH)
+
+#STEP: Detect OS
+#NOTE: Used to determine which OS is running.
+#NOTE: Currently it only has Debian and Arch Linux. In the future I may add more
+echo "Determining operating system"
+#NOTE: This is for archiving purposes
+##export OSINFO="$PWD/osinfo.sh"
+export OSINFO=bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/osinfo.sh)"
+echo "installed OS running is using $OSINFO"
+
+#STEP: Package manager
+#NOTE: Determines the package manage being used based on the OS
+echo "Determining package manager"
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/osinfo.sh)"
