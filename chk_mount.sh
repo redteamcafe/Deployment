6df7716841369b91d9151c4e8f3decc2b52063b5
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 00:44:52 -0400

    Update and rename chk_mount.sh to rm

--- chk_mount.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#STEP: Check mount location
-#NOTE: This was originally used when I would mount directory where the script lied on the server directly
-#NOTE: This is being kept for preservation purposes
-##SCRIPTDIR=$(readlink -f "$0")
-##SCRIPTPATH=$(dirname "$SCRIPTDIR")
-##AVIR=$(echo $SCRIPTPATH)
