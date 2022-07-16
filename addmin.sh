ea71964efea4d3144b291f741c6aa1e822157e70
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 01:23:41 -0400

    Update addmin.sh

--- addmin.sh
+++ addmin.sh
@@ -5,9 +5,7 @@
 
 UNAME=`whoami`
 OLD_UNAME=$UNAME
-echo "Changing default uname $UNAME"
 read -p "Enter the desired username >> " NEW_UNAME
-
-#NOTE: This changes the name of the default "arch" user to a desired one
+echo "Changing default uname $UNAME"
 sudo usermod -l $NEW_UNAME -m -d /home/$NEW_UNAME $OLD_UNAME
 sudo groupmod -n $NEW_UNAME $OLD_UNAME
