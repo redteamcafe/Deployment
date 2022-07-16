feaf43eb55c90bc29386ce5f3e43dc5003c8f20f
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 18:31:35 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,17 @@
 #STEP: Check mount location
 #NOTE: This was originally used when I would mount directory where the script lied on the server directly
 #NOTE: This is being kept for preservation purposes
-##SCRIPTDIR=$(readlink -f "$0")
-##SCRIPTPATH=$(dirname "$SCRIPTDIR")
-##PWD=$(echo $SCRIPTPATH)
+##export SCRIPTDIR=$(readlink -f "$0")
+##export SCRIPTPATH=$(dirname "$SCRIPTDIR")
+##export PWD=$(echo $SCRIPTPATH)
 
 #STEP: Detect OS
 #NOTE: Used to determine which OS is running.
 #NOTE: Currently it only has Debian and Arch Linux. In the future I may add more
 echo "Determining operating system"
 #NOTE: This is for archiving purposes
-##OSINFO="$PWD/osinfo.sh"
-OSINFO=`curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/osinfo.sh | bash`
+##export OSINFO="$PWD/osinfo.sh"
+export OSINFO=bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/osinfo.sh)"
 echo "installed OS running is using $OSINFO"
 
 #STEP: Package manager
@@ -25,7 +25,7 @@ echo "Determining package manager"
 curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/osinfo.sh | bash
 
 #STEP: Update and upgrade
-curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/update.sh | bash
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/update.sh)"
 
 #STEP Uname and hostname change (for Arch Linux)
 
@@ -33,15 +33,14 @@ if
 #apt-get
         [[ "$OSINFO" == "$APT" ]];
 then
-        curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/unname.sh | bash
-        curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/hostname.sh | bash
+        bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/hostname.sh)"
 else
         sleep 0
 fi
 
 #STEP Network Manager
 #NOTE: This step sets NetworkManager daemon as the primary network manager and removes systemd-networkd if it is installed
-curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/netman.sh | bash
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/network.sh)"
 
 #STEP Cockpit Web UI
 sudo $UPDATE ; sudo $INSTALL cockpit
@@ -53,7 +52,7 @@ then
 fi
 
 #STEP Install Docker and Containers
-curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/docker%20.sh | bash
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/deploy.sh)"
 
 
 
