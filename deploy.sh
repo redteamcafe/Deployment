df8b277a4ad2116ab75c1ed43bdfd4861411cb94
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 00:45:39 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,10 @@
 
 # This will help automate some of the deployment of severs
 # Currently supports Debian and Arch Linux
+
+#STEP: Check mount location
+#NOTE: This was originally used when I would mount directory where the script lied on the server directly
+#NOTE: This is being kept for preservation purposes
+##SCRIPTDIR=$(readlink -f "$0")
+##SCRIPTPATH=$(dirname "$SCRIPTDIR")
+##PWD=$(echo $SCRIPTPATH)
