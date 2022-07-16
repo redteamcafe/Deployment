af65dba3a8636b0a293e3dc6ad80476b658e9079
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 18:52:34 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -23,3 +23,33 @@ echo "installed OS running is using $OSINFO"
 #NOTE: Determines the package manage being used based on the OS
 echo "Determining package manager"
 echo "Package manager is $OSINFO"
+
+#STEP: Update and upgrade
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/update.sh)"
+
+#STEP Uname and hostname change (for Arch Linux)
+
+if
+#apt-get
+        [[ "$OSINFO" == "$APT" ]];
+then
+        bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/hostname.sh)"
+else
+        sleep 0
+fi
+
+#STEP Network Manager
+#NOTE: This step sets NetworkManager daemon as the primary network manager and removes systemd-networkd if it is installed
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/network.sh)"
+
+#STEP Cockpit Web UI
+sudo $UPDATE ; sudo $INSTALL cockpit
+if
+#apt-get
+        [[ "$OSINFO" == "$PAC" ]];
+then
+        sudo systemctl enable --now cockpit.socket
+fi
+
+#STEP Install Docker and Containers
+bash -c "$(curl -fsSL raw.githubusercontent.com/bobafett2010/Deployment/main/deploy.sh)"
