8da05f6811018ab9f00b53fed4037b51492ac115
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-17 17:16:16 -0400

    Create cockpit.sh

---
+++ cockpit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source vari
+
+#This installs the Cockpit Web UI
+
+$INSTALL cockpit
+while true; do
+  read -p "Do you want to install the Cockpit Web UI? >> " YN
+  case ${YN:0:1} in
+    [Yy]* )
+      if 
+        $UPDATE;
+        $INSTALL cockpit;
+        echo "Cockpit installation complete";
+      else
+        echo "Exiting setup...";
+      fi
+      break;;
+    [Nn]* )
+      break;;
+    * ) echo "Please answer either Y/y or N/n";;
+  esac
+done
+
+if
+#apt-get
+  [[ "$OSINFO" == "$PAC" ]];
+then
+  sudo systemctl enable --now cockpit.socket
+else
+  sleep 0
+fi
+
+echo "Finishing..."
