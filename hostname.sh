34674b534b8be5de71c94e06bb2288f143c8449d
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-16 20:12:43 -0400

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -3,4 +3,7 @@
 echo "changing hostname"
 read -p "what would you like to rename the hostname for this machine?" HOSTNAME
 echo $HOSTNAME > /etc/hostname
+echo "127.0.1.1 $HOSTNAME" > /etc/hosts
 echo "hostname updated"
+
+exit 0
