2fd671ea91447141d2d00c2ff6caf96b9e43e62e
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-17 16:52:14 -0400

    Update and rename hostname.sh to charch.sh

---
+++ charch.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source vari
+
+echo "Making changes to Arch Linux server"
+while true; do
+        read -p "Would you like to change the hostname for this server? (y=yes n=no) >> " YN
+        case ${YN:0:1} in
+                [Yy]* )
+                        read -p "What would you like to rename the host to? >> " HOST;
+                        while true; do
+                                read -p "Confirm hostname change $HOST. (y=yes n=no x=exit) >> " YN
+                                case ${YN:0:1} in
+                                        [Yy]* )
+                                                echo "Changing hostname!";
+                                                echo "Adding $HOST to /etc/hostname"
+                                                sudo echo $HOST > /etc/hostname;
+                                                STR=`grep '127.0.1.1' /etc/hosts | awk -F' ' '{print $2}'`;
+                                                echo "Changing $STR to $HOST in /etc/hosts"
+                                                sudo sed -i "s|$STR|$HOST|g" /etc/hosts;
+                                                break;;
+                                        [Nn]* )
+                                                read -p "What would you like to rename the host to? >> " HOST;;
+                                        [Xx]* )
+                                                echo "exiting";
+                                                break;;
+                                esac
+                        done
+                        break;;
+                [Nn]* ) 
+                        echo "moving on";
+                        break;;
+                * ) echo "Please answer either y or n";;
+        esac
+done
+
+#echo $HOSTNAME > /etc/hostname
+#echo "127.0.1.1 $HOSTNAME" > /etc/hosts
+#echo "hostname updated"
--- hostname.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "changing hostname"
-read -p "what would you like to rename the hostname for this machine?" HOSTNAME
-echo $HOSTNAME > /etc/hostname
-echo "127.0.1.1 $HOSTNAME" > /etc/hosts
-echo "hostname updated"
-
-exit 0
