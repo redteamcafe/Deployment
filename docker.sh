8dd0cca2fcee7f0b18898cbce8cf0d32a5f5a26e
Christian McLaughlin <mandalorianmail@protonmail.com>
2022-07-23 22:08:45 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -16,9 +16,10 @@ while true; do
         sudo apt update;
         sudo apt install ca-certificates curl gnupg lsb-release;
         sudo mkdir -p /etc/apt/keyrings;
-        curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
+        RELEASE=$(lsb_release -a | grep -o -i "ubuntu\|debian" | uniq | tr [:upper:] [:lower:]);
+        curl -fsSL https://download.docker.com/linux/$RELEASE/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg;
         echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$RELEASE \
         $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
         sudo apt update;
         sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin;
